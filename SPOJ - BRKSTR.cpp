22b256e1b9fbe568c70f026ce4f77efeb77472f0
Mahmudul-Kabir <59996575+Mahmudul-Kabir@users.noreply.github.com>
2022-03-09 01:06:31 +0600

    Create SPOJ - BRKSTR.cpp

---
+++ SPOJ - BRKSTR.cpp
@@ -0,0 +1,6 @@
+/*
+https://github.com/Mahmudul-Kabir/cp-solutions/blob/main/UVA%2010003%20Fully%20OC.cpp
+
+exact same code. Just tweak the dp[][] and cut[][] size and change the output format. 
+
+*/
